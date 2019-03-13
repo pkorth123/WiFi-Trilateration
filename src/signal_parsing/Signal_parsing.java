9a6d03bf72331b42e89f6f0b471822a57fcd9109
Pat <Pat@Andromeda>
2019-03-13 01:38:34 -0400

    First commit, printing seconds and RSSI from tcpdump

--- src/signal_parsing/Signal_parsing.java
+++ src/signal_parsing/Signal_parsing.java
@@ -29,13 +29,16 @@ public class Signal_parsing {
         
         int portNumber = 5432;
 
-        Socket liveDump = new Socket("192.168.1.59", portNumber);
+        Socket liveDump = new Socket("192.168.1.57", portNumber);
         PrintWriter out = new PrintWriter(liveDump.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(liveDump.getInputStream()));
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-
+       // BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(liveDump.getOutputStream()));
+        //wr.write("tcpdump -lvvne -s0 -i wlan0 wlan type mgt and wlan[0]!=0x80All");
+        
 //        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Pat\\Desktop\\hello\\02-chan1-back.txt"));   
         String line;
+     
         boolean infinite = true;
         while (infinite == true) {
                     System.out.println("here1");
@@ -56,6 +59,8 @@ public class Signal_parsing {
                 distance = Math.pow(10, ((10 + (-signalStrength / i)) / (10 * n)));
                 System.out.print("at " + seconds + " seconds" + " the signal strength was ");
                 System.out.print(signalStrength / i + "dBm, ");
+                System.out.print(i + "samples, ");
+
                 System.out.println("This is associated with a distance of " + distance + " meters given an n-value of " + n + ".");
                 System.out.println();
             }
