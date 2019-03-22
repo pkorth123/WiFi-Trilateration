d3fd657bfb15450c71cd588e88abead052289d10
Pat <Pat@Andromeda>
2019-03-21 21:43:55 -0400

    get distance broken out as separate method

--- src/signal_parsing/Signal_parsing.java
+++ src/signal_parsing/Signal_parsing.java
@@ -7,7 +7,6 @@ package signal_parsing;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,56 +24,21 @@ public class Signal_parsing {
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         int portNumber = 5432;
-
-        Socket liveDump = new Socket("192.168.1.57", portNumber);
-        PrintWriter out = new PrintWriter(liveDump.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(liveDump.getInputStream()));
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-       // BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(liveDump.getOutputStream()));
-        //wr.write("tcpdump -lvvne -s0 -i wlan0 wlan type mgt and wlan[0]!=0x80All");
-        
-//        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Pat\\Desktop\\hello\\02-chan1-back.txt"));   
+        String host = "192.168.1.59";
         String line;
-     
-        boolean infinite = true;
-        while (infinite == true) {
-                    System.out.println("here1");
-            while ((line = in.readLine()) != null) {
-                System.out.println("here2");
-          //      Thread.sleep(100);
-                float signalStrength = 0;
-                float seconds = getSecond(line);
-                int i = 0;
-                while (((line = in.readLine()) != null) && (getSecond(line) < (seconds + 1)) && (getSecond(line) >= seconds)) {
-                    i++;
-                    signalStrength += getSignalStrength(line);
-                    line = in.readLine();
-                }
-
-                double distance = 0;
-                double n = 4;
-                distance = Math.pow(10, ((10 + (-signalStrength / i)) / (10 * n)));
-                System.out.print("at " + seconds + " seconds" + " the signal strength was ");
-                System.out.print(signalStrength / i + "dBm, ");
-                System.out.print(i + "samples, ");
-
-                System.out.println("This is associated with a distance of " + distance + " meters given an n-value of " + n + ".");
-                System.out.println();
-            }
-        }
+        getLines(host, portNumber);
     }
-
-    // parse signal strength out
+        // parse signal strength out
     public static float getSignalStrength(String line) {
         float signal = 0;
-        Pattern p = Pattern.compile("[-][0-9]*");
+        if (line != null){
+        Pattern p = Pattern.compile("[-][0-9][0-9]");
         Matcher m = p.matcher(line);
         m.find();
         signal = Float.parseFloat(m.group());
-
+        }
         return signal;
     }
 //parse out seconds from time stamp
@@ -88,4 +52,23 @@ public class Signal_parsing {
         return second;
     }
 
+    public static String getLines(String host, int portNumber) throws IOException {
+
+        Socket liveDump = new Socket(host, portNumber);
+        BufferedReader in = new BufferedReader(new InputStreamReader(liveDump.getInputStream()));
+        String line = null;
+
+        while ((line = in.readLine()) != null) {
+            line = in.readLine();
+            System.out.println(getSignalStrength(line));
+        }
+        return null;
+    }
+
+    public static float getDistance(int signalStrength, float n) {
+        float distance = 0;
+        distance = (float) Math.pow(10, ((10 + (-signalStrength)) / (10 * n)));
+        return distance;
+
+    }
 }
