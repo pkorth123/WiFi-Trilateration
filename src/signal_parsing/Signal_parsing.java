c3081d13c9fe23928d1766506d014bb546b8e2de
Patrick <pkorth123@gmail.com>
2019-03-30 23:30:15 -0400

    get distance broken out as separate method

--- src/signal_parsing/Signal_parsing.java
+++ src/signal_parsing/Signal_parsing.java
@@ -9,9 +9,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,29 +23,73 @@ public class Signal_parsing {
 //removed main method, should only have to call getDistance() to start printing values
 
     public static void main(String[] args) throws IOException {
-        new Thread(() -> {
-            try {
-               float dist57 = getDistance("192.168.1.57", 5432, 6);
-            } catch (IOException ex) {
-                Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+        Signal_parsing parse = new Signal_parsing();
+        parse.getThreads();
+    }
+
+    public void getThreads() {
+        Semaphore one = new Semaphore(1);
+        Semaphore two = new Semaphore(0);
+        Semaphore three = new Semaphore(0);
+
+        Thread router57 = new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    try {
+                        one.acquire();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    float dist57 = getDistance("192.168.1.57", 5457, 6);
+                    System.out.println(dist57 + "   57");
+                    two.release();
+                } catch (IOException ex) {
+                    Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-        }).start();
-        new Thread(() -> {
-            try {
-                float dist58 = getDistance("192.168.1.58", 5433, 6);
-            } catch (IOException ex) {
-                Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+
+        });
+        Thread router58 = new Thread(new Runnable() {
+            //add if statements so that when all three value are not void it calcs a point
+            public void run() {
+                try {
+                    try {
+                        two.acquire();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    float dist58 = getDistance("192.168.1.58", 5458, 6);
+                    System.out.println(dist58 + "   58");
+                    three.release();
+                } catch (IOException ex) {
+                    Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-        }).start();
-        new Thread(() -> {
-            try {
-                float dist59 = getDistance("192.168.1.59", 5434, 6);
-            } catch (IOException ex) {
-                Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+
+        });
+        Thread router59 = new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    try {
+                        three.acquire();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    float dist59 = getDistance("192.168.1.59", 5459, 6);
+                    System.out.println(dist59 + "   59");
+                    one.release();
+                } catch (IOException ex) {
+                    Logger.getLogger(Signal_parsing.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-        }).start();
-        
-     //need some sorta statements here to start to match up out distances to times/ips
+
+        });
+        router57.start();
+        router58.start();
+        router59.start();
+
     }
 
     public static float getSignalStrength(String line) {
@@ -87,7 +131,7 @@ public class Signal_parsing {
 
             }
             distance = (float) Math.pow(10, ((10 + (-(signal))) / (10 * n)));
-            System.out.println(distance + "   " + host);
+            System.out.println(distance + "  " + host);
         }
         return distance;
 
