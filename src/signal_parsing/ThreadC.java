e46fac425a7010fbe6fb5c954521a770bf6dbae9
Patrick <pkorth123@gmail.com>
2019-04-03 23:56:32 -0400

    printing threads in order

--- src/signal_parsing/ThreadC.java
+++ src/signal_parsing/ThreadC.java
@@ -5,6 +5,13 @@
  */
 package signal_parsing;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ThreadC extends Thread {
 
     ResourceLock lock;
@@ -15,22 +22,37 @@ public class ThreadC extends Thread {
 
     @Override
     public void run() {
-
         try {
-            synchronized (lock) {
-                if ((lock.flag !=3)) {
-                    lock.wait();
-                } else {
-                    float dist59 = parseSignal.getDistance("192.168.1.59", 5459, 6);
-                    System.out.println(dist59 + "   59");
-                    Thread.sleep(1000);
-                    lock.flag = 1;
-                    lock.notifyAll();
+            float dist59 = 0;
+            int n = 6;
+            Socket liveDump57 = new Socket("192.168.1.59", 5459);
+            BufferedReader in = new BufferedReader(new InputStreamReader(liveDump57.getInputStream()));
+            String line = in.readLine();
+            while (true) {
+                line =in.readLine();
+                try {
+                    synchronized (lock) {
+                        
+                        if (lock.flag != 3) {
+                            lock.wait();
+                        } else {
+                            
+                            dist59 = (float) Math.pow(10, ((10 - parseSignal.getSignalStrength(line)) / (10 * n)));
+                            System.out.println(dist59 + "   59");
+                            Thread.sleep(1000);
+                            lock.flag = 1;
+                            lock.notifyAll();
+                        }
+                        
+                    }
+                } catch (Exception e) {
+                    System.out.println("Exception 1 :" + e.getMessage());
                 }
-            }
-        } catch (Exception e) {
-            System.out.println("Exception 3 :" + e.getMessage());
-        }
 
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ThreadA.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
 }
