1316410046a02c9ba80fba0c7fed41d1f52eede2
Patrick <pkorth123@gmail.com>
2019-04-18 00:29:39 -0400

    finally printing coordinates, albeit all fucked up

--- src/signal_parsing/Parser.java
+++ src/signal_parsing/Parser.java
@@ -3,14 +3,15 @@ package signal_parsing;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-
+import java.io.File;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 /**
  *
@@ -18,7 +19,8 @@ import javax.swing.JPanel;
  */
 public class Parser {
 
-    //variables for "getCoordinates"
+    static int width = 1920;
+    static int height = 1080;
     Point2D p1 = new Point2D.Float(0, 0);
     Point2D p2 = new Point2D.Float((float) 6.5, 0);
     Point2D p3 = new Point2D.Float((float) 3.85, (float) 5.8);
@@ -29,30 +31,30 @@ public class Parser {
     static double E = ((-2 * y2) + (2 * y3));
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        final BufferedImage image = new BufferedImage(1280, 768,
-                BufferedImage.TYPE_INT_RGB);
+
+        JFrame frame = new JFrame();
+        File f = new File("C:\\Users\\Pat\\Desktop\\burning.jpg");
+        final BufferedImage img = ImageIO.read(f);
+        
         JPanel canvas = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(image, 0, 0, this);
+                g.drawImage(img, 0, 0, this);
             }
         };
 
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());   // <== make panel fill frame
-        frame.add(canvas, BorderLayout.CENTER);
-        frame.setSize(1000, 1000);
+        BackgroundPanel background = new BackgroundPanel(img, BackgroundPanel.ACTUAL);
+        frame.setContentPane(background);
+        frame.add(canvas);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setSize(1920, 1080);
         frame.setVisible(true);
 
-        // do you drawing somewhere else, maybe a different thread
-        //g.fillOval(500, 500 ,50, 50);
         ResourceLock lock = new ResourceLock();
-
         ThreadA a = new ThreadA(lock);
         ThreadB b = new ThreadB(lock);
         ThreadC c = new ThreadC(lock);
-
         a.start();
         a.sleep(100);
         b.start();
@@ -61,30 +63,27 @@ public class Parser {
         c.sleep(100);
         double x;
         double y;
+        int mult = 100;//scale output points to fit frame dimensions
         while (true) {
-            float dist57 = a.getDist57();
-
+            double dist57 = a.getDist57();
             a.sleep(100);
-            // System.out.println(dist57);
 
-            float dist58 = b.getDist58();
-
+            double dist58 = b.getDist58();
             b.sleep(100);
-            // System.out.println(dist58);
 
-            float dist59 = c.getDist59();
-
+            double dist59 = c.getDist59();
             c.sleep(100);
-            //System.out.println(dist59);
+
             Thread.sleep(700);
-            x = (Parser.getCoordinates(dist57, dist58, dist59).getX()) * 100;
-            y = (Parser.getCoordinates(dist57, dist58, dist59).getY()) * 100;
-            System.out.println("(" + x + ", " + y + ")");
-            Graphics g = image.getGraphics();
-            g.setColor(Color.red);
+            x = ((Parser.getCoordinates(dist57, dist58, dist59).getX()) * mult);
+            y = ((Parser.getCoordinates(dist57, dist58, dist59).getY()) * mult);
+            System.out.println("(" + (x) + ", " + (y) + ")");
+                    Graphics g = img.getGraphics(); g.setColor(Color.red);
+            
             g.fillOval((int) x, (int) y, 50, 50);
             g.dispose();
             canvas.repaint();
+
         }
 
     }
@@ -99,15 +98,7 @@ public class Parser {
         return signal;
     }
 
-    String line;
-
-    public String getLine() {
-        return line;
-    }
-
-    float distance;
-
-    public static Point2D getCoordinates(float d1, float d2, float d3) {
+    public static Point2D getCoordinates(double d1, double d2, double d3) {
         double C = ((Math.pow(d1, 2)) - (Math.pow(d2, 2)) - (Math.pow(x1, 2)) + (Math.pow(x2, 2)) - (Math.pow(y1, 2)) + (Math.pow(y2, 2)));
         double F = ((Math.pow(d2, 2)) - (Math.pow(d3, 2)) - (Math.pow(x2, 2)) + (Math.pow(x3, 2)) - (Math.pow(y2, 2)) + (Math.pow(y3, 2)));
         double x = 0;
@@ -119,10 +110,13 @@ public class Parser {
         return coordinates;
     }
 
-}
+    String line;
 
-//parse out seconds from time stamp
-/*
+    public String getLine() {
+        return line;
+    }
+    //parse out seconds from time stamp for testing
+
     public static float getSecond(String line) {
         float second = 0;
         Pattern p = Pattern.compile("[0-9][0-9][.][0-9]*");
@@ -131,4 +125,5 @@ public class Parser {
         second = Float.parseFloat(m.group());
         return second;
     }
- */
+
+}
